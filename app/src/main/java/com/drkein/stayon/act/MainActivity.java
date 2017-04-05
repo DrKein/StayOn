9d0a8550f0026070f88421ef60fbd39cc8d62553
kein <kein.you@gmail.com>
2017-04-05 13:43:17 +0900

    change layout file name.

--- app/src/main/java/com/drkein/stayon/act/MainActivity.java
+++ app/src/main/java/com/drkein/stayon/act/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        setContentView(R.layout.activity_main);
 
         Switch manualSwitch = (Switch)findViewById(R.id.manualSwitch);
         manualSwitch.setChecked(Pref.getServiceRunning(this));
